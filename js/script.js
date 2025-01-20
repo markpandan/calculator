9c970f4f629968a866ad095f2c12aed4d66c8696
markpandan <72681401+markpandan@users.noreply.github.com>
2025-01-21 00:13:43 +0800

    Fix chaining calculations
    - Added several undefined properties for the num2 variable inside
    script.js to ensure that there are no unintended computations will happen.

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ calculator_buttons.addEventListener("click", (e) => {
       if (num1) num2 = text;
       else num2 = num1;
       text = operate(num1, num2, operator);
+      operator = "";
       operatorPress = true;
       break;
     case "clear":
@@ -32,21 +33,26 @@ calculator_buttons.addEventListener("click", (e) => {
       else text = "-" + text;
       break;
     case "decimal":
-      if (!text.includes(BUTTON_ACTIONS[buttonPress]))
+      if (!text.includes(BUTTON_ACTIONS[buttonPress])) {
         text += BUTTON_ACTIONS[buttonPress] ?? "";
+        operatorPress = false;
+      }
       break;
     default:
       if (OPERATORS.includes(BUTTON_ACTIONS[buttonPress])) {
         operator = BUTTON_ACTIONS[buttonPress];
+        num2 = undefined;
         if (num1 && num2) {
           text = operate(num1, num2, operator);
         } else {
           num1 = text;
           operatorPress = true;
         }
-        // Clears the text when the user presses operators, equal signs and will let you save the previous value
+        // Clears the text when the user presses operators, equal signs, and decimal points.
+        // Also, it will let you save the previous value
       } else if (operatorPress) {
         num1 = text;
+        num2 = undefined;
         text = BUTTON_ACTIONS[buttonPress] ?? "";
         operatorPress = false;
       } else text += BUTTON_ACTIONS[buttonPress] ?? "";
