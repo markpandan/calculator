2c5c78b9f9ee6887cf5b92ed6d4ec21c44230637
markpandan <72681401+markpandan@users.noreply.github.com>
2025-01-20 20:21:36 +0800

    Implement calculator operations
    - Completed the functionality for add, subtract, multiple, divide, and
    modulo
    - The code might get confusing, thus the next update should be tasked
    for refractoring.
    - Placed all of the javascript files inside the 'js' folder

--- script.js
+++
@@ -1,8 +0,0 @@
-// import { add, subtract, multiply, divide } from "./operations";
-import { BUTTON_ACTIONS } from "./constants.js";
-
-let display = document.querySelector(".display");
-let calculator_buttons = document.querySelector("#calculator-buttons");
-calculator_buttons.addEventListener("click", (e) => {
-  display.textContent += BUTTON_ACTIONS[e.target.id] ?? "";
-});
